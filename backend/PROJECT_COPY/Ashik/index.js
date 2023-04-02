0ec778e9de01c0fc5e5088a20b1586a6357bb62f
vishalpadaswan3 <vishalpadaswan1.com>
2023-04-02 23:11:47 +0530

    Done

--- backend/PROJECT_COPY/Ashik/index.js
+++ backend/PROJECT_COPY/Ashik/index.js
@@ -4,6 +4,7 @@ const socketio = require("socket.io");
 const mongoose = require("mongoose");
 var randomId = require("random-id");
 const { User, update_word_function } = require("../user");
+let { users } = require("../user");
 
 // length of the id (default is 30)
 var len = 10;
@@ -30,15 +31,9 @@ const io = socketio(expressServer);
 
 // here is my ranodom paragraph
 let para = [
-  "Hello Guys my name is mohammad ashik ansari and  i am from the jharkhand state of india",
-  "this is our contruct week project guys in this we will be making the clone of typeracer website",
-  "Hii this is my laptop and it has 8 gb of ram",
-  "kaisa hai bhai log sab let's make our project that we have got during our construct week",
-  "the ipl season 17 is going to be started from 30th of march 2023",
-  "Chennai super kings is my favourite ipl team this year what about you all guys",
-  "I have the highest typing record of 60 wpm",
+  "So, I was essentially a "
 ];
-
+// So, I was essentially a backbencher or some kind of outcast there. Feeling a bit dejected, I wanted to prove something my worth to them. So, I started to wait for an opportunity.
 function myFunction() {
   let random = Math.floor(Math.random() * para.length);
   return random;
@@ -52,25 +47,39 @@ io.on("connection", (socket) => {
   count += 1;
 
   socket.on("username", ({ username }) => {
+
     var id = randomId(len, pattern);
     console.log(id);
     socket.emit("roomno", id);
   });
+let Room;
   socket.on("joinroom", ({ username, roomvalue }) => {
     const user = User(socket.id, username, roomvalue);
     console.log(roomvalue + "from join room");
     console.log(socket.id + "from line no 68");
     socket.join(roomvalue);
-    socket.emit("message", "welcome to race");
+    Room=roomvalue;
+    io.emit("usersarray", users)
+    socket.emit("message", "WELCOME TO RACE BUDDY 😉");
   });
   console.log(`One user connected, total user : ${count}`);
 
+  socket.on("timeleft", (data) => {
+   let {timeleft}=data;
+   socket.broadcast.to(Room).emit("Time",{timeleft})
+  });
+  io.emit('user count', count);
+
+  socket.on("display", (data) => {
+    socket.broadcast.to(Room).emit("forall", data);
+  });
+
   //emitting the paragraph
-  let myParagraph = para[myFunction()];
+  let myParagraph = para[0];
   socket.emit("thePara", myParagraph);
 
   //recieving the typed text from client
-  socket.on("typedText", ({ typedText, keyCode, flag }) => {
+  socket.on("typedText", ({ typedText }) => {
     console.log(`person having id ${socket.id} is typing :`, typedText);
 
     if (
@@ -79,21 +88,25 @@ io.on("connection", (socket) => {
     ) {
       if (typedText.length == myParagraph.length) {
         console.log(typedText);
+        // users = []
         return socket.emit("typing-update", {
-          typedText: "You have finished the race",
-          flag,
+          typedText: "You have finished the race buddy 👏👏👏",
+          flag: "Race Completed",
         });
       }
       if (typedText[typedText.length - 1] == " ") {
         let user = update_word_function(socket.id, typedText);
         console.log(user);
-        io.to(user[0].roomvalue).emit("user_data", {
-          userData: user[0],
-        });
+        console.log(user[0])
+        io.to(user[0].roomvalue).emit("user_data",
+          user[0],
+
+        );
       }
-      console.log({ typedText, keyCode });
+      // console.log({ typedText, keyCode });
       socket.emit("typing-update", {
         typedText,
+
         isTyping: true,
         socketID: socket.id,
         flag: true,
@@ -113,6 +126,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     count -= 1;
     console.log(`One user left, ${count} remaining!!`);
+    io.emit('user count', count);
   });
 });
 
@@ -124,3 +138,5 @@ const includeFunction = (myParagraph, typedText) => {
     return false;
   }
 };
+
+module.exports = { count }
