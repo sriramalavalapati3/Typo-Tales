2f24e2a09e4360b034212414b3f401b853c24a08
sriram <112754538+sriramalavalapati3@users.noreply.github.com>
2023-04-07 00:02:53 +0530

    Delete db.js

--- backend/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
-const connection = mongoose.connect("mongodb+srv://saurabh:saurabh@cluster0.hovcp.mongodb.net/Typing_Tales?retryWrites=true&w=majority")
-
-module.exports= {connection}
